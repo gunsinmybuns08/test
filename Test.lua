99a2603b97b263500e5594a04ecfec588fa0aa04
Bigmilk <74319053+gunsinmybuns08@users.noreply.github.com>
2021-12-20 15:36:34 -0500

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -4,29 +4,22 @@ function library:CreateForm(name)
 	local ScreenGui = Instance.new("ScreenGui")
 	local Container = Instance.new("Frame")
 	local TabHolder = Instance.new("Frame")
-	local UICornerGUI = Instance.new("UICorner")
-	local TabUICorner = Instance.new("UICorner")
+
 
 	ScreenGui.Parent = game.CoreGui
 	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-	Container.Name = name
+	Container.Name = "Container"
 	Container.Parent = ScreenGui
 	Container.BackgroundColor3 = Color3.fromRGB(29, 26, 34)
 	Container.Position = UDim2.new(0.169373542, 0, 0.096339114, 0)
 	Container.Size = UDim2.new(0, 570, 0, 419)
-	
-	UICornerGUI.Parent = Container
-	UICornerGUI.CornerRadius = 8
 
 	TabHolder.Name = "TabHolder"
-	TabHolder.Parent = game.StarterGui.ScreenGui[name]
+	TabHolder.Parent = Container
 	TabHolder.BackgroundColor3 = Color3.fromRGB(15, 13, 18)
 	TabHolder.BorderSizePixel = 0
 	TabHolder.Position = UDim2.new(0.0263157897, 0, 0.0286396183, 0)
 	TabHolder.Size = UDim2.new(0, 96, 0, 394)
-	
-	TabUICorner.Parent = TabHolder
-	TabUICorner.CornerRadius = 6
 end
 return library
